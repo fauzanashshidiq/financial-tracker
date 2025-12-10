27654fd6d676f3776ef999142a8ce343ea6670a9
fauzanashshidiq <muhzeen07@gmail.com>
2025-12-10 16:10:17 +0700

    feat(dashboard-layout): add breadcumb

--- frontend-vite/src/components/layouts/DashboardLayout.jsx
+++ frontend-vite/src/components/layouts/DashboardLayout.jsx
@@ -3,12 +3,31 @@ import {
   SidebarTrigger,
   SidebarInset,
 } from "@/components/ui/sidebar";
-
 import { AppSidebar } from "@/components/AppSidebar";
+import { useLocation, Link } from "react-router-dom";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+
+const breadcrumbLabels = {
+  "/dashboard": "Dashboard",
+  "/transaksi": "Transaksi",
+  "/budget": "Smart Budget Assistant",
+  "/profile": "Profile",
+};
 
 export function DashboardLayout({ children }) {
   const saved = localStorage.getItem("sidebar-open");
   const defaultOpen = saved === null ? true : saved === "true";
+
+  const location = useLocation();
+  const path = location.pathname;
+  const title = breadcrumbLabels[path] || "Menu";
 
   return (
     <SidebarProvider defaultOpen={defaultOpen}>
@@ -16,8 +35,18 @@ export function DashboardLayout({ children }) {
 
       <SidebarInset>
         <header className="flex py-4 items-center gap-2 px-4 border-b bg-white">
-          <SidebarTrigger />
-          <h2 className="font-semibold">Dashboard</h2>
+          <div className="flex items-center gap-2">
+            <SidebarTrigger />
+
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{title}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
         </header>
 
         <div className="p-4">{children}</div>
