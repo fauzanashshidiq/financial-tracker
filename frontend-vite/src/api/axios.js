6d07508885453dbe57a9ebaeb8b5f9d2cb8a3309
fauzanashshidiq <muhzeen07@gmail.com>
2025-12-11 18:06:52 +0700

    change baseURL backend for api

--- frontend-vite/src/api/axios.js
+++ frontend-vite/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://financial-tracker-chi-puce.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
